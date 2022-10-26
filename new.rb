d6ed4f26138022e8fc2d9ade1c05d919f35bb929
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2022-10-26 05:44:55 +0000

    [third]

--- new.rb
+++ new.rb
@@ -17,17 +17,16 @@ car.run(5)
 
 # クラスメソッド
 class Car
-  def self.run(distance)
-    puts "車で#{distance}キロ走ります。"
-  end
-end
-
-Car.run(10)
-
-class Car
   def self.turn(direction)
     puts "#{direction}に曲がります。"
   end
 end
 
 Car.turn("右")
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
